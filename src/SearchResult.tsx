788627a3b5c19e047ab7919868c5df8424f6040b
Dave Cohen <scraggo@gmail.com>
2023-04-26 21:01:51 -0500

    Add more result properties:
    - move result to its own component
    - make shared Result type
    - hook returns Result type
    - add more properties, style improvements

---
+++ src/SearchResult.tsx
@@ -0,0 +1,89 @@
+// import * as React from 'react';
+import { Box, Flex, Icon, Link, Text } from '@chakra-ui/react';
+import { FaUserCircle } from 'react-icons/fa';
+
+import { Result } from './types';
+
+type Props = {
+  colorMode: string;
+  isMatch: boolean;
+  result: Result;
+};
+
+const bottomTextMargin = 3;
+
+export default function SearchResult({ colorMode, isMatch, result }: Props) {
+  const { package: pkg } = result;
+  const {
+    author,
+    date,
+    description,
+    keywords,
+    links,
+    name,
+    publisher,
+    version,
+  } = pkg;
+  const { npm } = links;
+  const { username } = publisher;
+
+  return (
+    <Box
+      as="section"
+      borderBottom="1px solid"
+      borderBottomColor="gray.100"
+      pb={2}
+      mb={2}
+      key={name}
+    >
+      <Box>
+        <Link fontSize="xl" fontWeight="bold" href={npm} mb={bottomTextMargin}>
+          {name}
+        </Link>
+        {isMatch ? (
+          <Text
+            as="span"
+            p={1}
+            ml={6}
+            bgColor={colorMode === 'light' ? 'purple.50' : 'purple.800'}
+          >
+            exact match
+          </Text>
+        ) : null}
+      </Box>
+      <Text mb={bottomTextMargin}>{description}</Text>
+      {keywords && keywords.length > 0 ? (
+        <Box mb={bottomTextMargin}>
+          {keywords.map((keyword) => (
+            <Text
+              as="span"
+              key={keyword}
+              p={1}
+              mr={1}
+              mb={bottomTextMargin}
+              bgColor={colorMode === 'light' ? 'gray.50' : 'gray.600'}
+            >
+              {keyword}
+            </Text>
+          ))}
+          <br />
+        </Box>
+      ) : null}
+      {author ? (
+        <Flex my={1} alignItems="center">
+          <Icon as={FaUserCircle} />
+          <Link
+            fontWeight="bold"
+            href={`https://www.npmjs.com/~${username}`}
+            mx={1}
+          >
+            {author.name}
+          </Link>
+          <Text as="span">
+            published {version} • {date.split('T')[0]}
+          </Text>
+        </Flex>
+      ) : null}
+    </Box>
+  );
+}
--- src/SearchResults.tsx
+++ src/SearchResults.tsx
@@ -1,23 +1,13 @@
 // import * as React from 'react';
-import { Box, Link, Spinner, Text } from '@chakra-ui/react';
+import { Box, Spinner, Text, useColorModeValue } from '@chakra-ui/react';
 
 import { useNPMSearch } from './hooks';
+import SearchResult from './SearchResult';
 
 type Props = {
   searchText: string;
   shouldForceError: boolean;
 };
-
-interface Result {
-  package: {
-    description: string;
-    name: string;
-    version: string;
-    links: {
-      npm: string;
-    };
-  };
-}
 
 const Wrapper = ({ children }: { children?: any }) => (
   <Box as="main" mt={6}>
@@ -31,6 +21,7 @@ export default function SearchResults({ searchText, shouldForceError }: Props) {
     error,
     isLoading,
   } = useNPMSearch(shouldForceError ? '' : searchText);
+  const colorMode = useColorModeValue('light', 'dark');
 
   if (isLoading) {
     return (
@@ -47,30 +38,16 @@ export default function SearchResults({ searchText, shouldForceError }: Props) {
 
   return (
     <Wrapper>
-      <Text as="h2" fontWeight="bold" fontSize="xl" mb={6}>
+      <Text as="h2" fontWeight="bold" fontSize="2xl" mb={6}>
         {data.length} packages found
       </Text>
-      {data.map((result: Result) => {
-        const { package: pkg } = result;
-        const { description, links, name } = pkg;
-        const { npm } = links;
-        return (
-          <Box
-            as="section"
-            borderBottom="1px solid"
-            borderBottomColor="gray.100"
-            pb={2}
-            mb={2}
-            key={name}
-          >
-            <Text as="b">
-              <Link href={npm}>{name}</Link>
-            </Text>
-            <br />
-            <Text>{description}</Text>{' '}
-          </Box>
-        );
-      })}
+      {data.map((result) => (
+        <SearchResult
+          colorMode={colorMode}
+          isMatch={searchText === result.package.name}
+          result={result}
+        />
+      ))}
     </Wrapper>
   );
 }
